272ae92d36683f4cc639048e95b55412d505c336
Feroz <78134661+hrferoz@users.noreply.github.com>
2025-10-19 02:34:05 +0600

    Update CentOS repository configuration in repo.sh

--- repo.sh
+++ repo.sh
@@ -2,34 +2,44 @@
 
 echo "grep -q 'nameserver 8.8.8.8' /etc/resolv.conf || echo 'nameserver 8.8.8.8' >> /etc/resolv.conf" >> /etc/rc.local
 grep -q 'nameserver 8.8.8.8' /etc/resolv.conf || echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
-centosV=$(grep -oE '[0-9]+\.[0-9]+' /etc/redhat-release)
-
 > /etc/yum.repos.d/CentOS-Base.repo
 
 echo " 
+# CentOS-EOL.repo
+#
+# This is an example config which can be used to deal with EOL RHEL
+# You MUST look to mirroring this locally if you want long term access
+#
+
 [base]
 name=CentOS-$releasever - Base
-baseurl=http://archive.kernel.org/centos-vault/$centosV/os/$basearch/
+baseurl=http://archive.kernel.org/centos-vault/7.9.2009/os/$basearch/
+enabled=1
 gpgcheck=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
 
+#released updates
 [updates]
 name=CentOS-$releasever - Updates
-baseurl=http://archive.kernel.org/centos-vault/$centosV/updates/$basearch/
+baseurl=http://archive.kernel.org/centos-vault/7.9.2009/updates/$basearch/
+enabled=1
 gpgcheck=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
 
+#additional packages that may be useful
 [extras]
 name=CentOS-$releasever - Extras
-baseurl=http://archive.kernel.org/centos-vault/$centosV/extras/$basearch/
+baseurl=http://archive.kernel.org/centos-vault/7.9.2009/extras/$basearch/
+enabled=1
 gpgcheck=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
 
+#additional packages that extend functionality of existing packages
 [centosplus]
 name=CentOS-$releasever - Plus
-baseurl=http://archive.kernel.org/centos-vault/$centosV/centosplus/$basearch/
-gpgcheck=1
+baseurl=http://archive.kernel.org/centos-vault/7.9.2009/centosplus/$basearch/
 enabled=0
+gpgcheck=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
 " >> /etc/yum.repos.d/CentOS-Base.repo
 
