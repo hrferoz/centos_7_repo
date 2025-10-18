681f92bb60b495c1e1aa9496bfc741026b566378
Feroz <78134661+hrferoz@users.noreply.github.com>
2025-10-19 02:29:23 +0600

    Update CentOS repository URLs fix

--- repo.sh
+++ repo.sh
@@ -9,35 +9,35 @@ centosV=$(grep -oE '[0-9]+\.[0-9]+' /etc/redhat-release)
 echo " 
 [base]
 name=CentOS-$releasever - Base
-baseurl=http://archive.kernel.org/$centosV/os/$basearch/
+baseurl=http://archive.kernel.org/centos-vault/$centosV/os/$basearch/
 gpgcheck=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
 
 [updates]
 name=CentOS-$releasever - Updates
-baseurl=http://archive.kernel.org/$centosV/updates/$basearch/
+baseurl=http://archive.kernel.org/centos-vault/$centosV/updates/$basearch/
 gpgcheck=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
 
 [extras]
 name=CentOS-$releasever - Extras
-baseurl=http://archive.kernel.org/$centosV/extras/$basearch/
+baseurl=http://archive.kernel.org/centos-vault/$centosV/extras/$basearch/
 gpgcheck=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
 
 [centosplus]
 name=CentOS-$releasever - Plus
-baseurl=http://archive.kernel.org/$centosV/centosplus/$basearch/
+baseurl=http://archive.kernel.org/centos-vault/$centosV/centosplus/$basearch/
 gpgcheck=1
 enabled=0
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
 " >> /etc/yum.repos.d/CentOS-Base.repo
 
 sed -i 's|name=CentOS-|name=CentOS-$releasever|g' /etc/yum.repos.d/CentOS-Base.repo
-sed -i 's|baseurl=http://archive.kernel.org/.*./os//|baseurl=http://archive.kernel.org/7.9.2009/os/$basearch/|g' /etc/yum.repos.d/CentOS-Base.repo
-sed -i 's|baseurl=http://archive.kernel.org/.*./updates//|baseurl=http://archive.kernel.org/7.9.2009/updates/$basearch/|g' /etc/yum.repos.d/CentOS-Base.repo
-sed -i 's|baseurl=http://archive.kernel.org/.*./extras//|baseurl=http://archive.kernel.org/7.9.2009/extras/$basearch/|g' /etc/yum.repos.d/CentOS-Base.repo
-sed -i 's|baseurl=http://archive.kernel.org/.*./centosplus//|baseurl=http://archive.kernel.org/7.9.2009/centosplus/$basearch/|g' /etc/yum.repos.d/CentOS-Base.repo
+sed -i 's|baseurl=http://archive.kernel.org/centos-vault/.*./os//|baseurl=http://archive.kernel.org/centos-vault/7.9.2009/os/$basearch/|g' /etc/yum.repos.d/CentOS-Base.repo
+sed -i 's|baseurl=http://archive.kernel.org/centos-vault/.*./updates//|baseurl=http://archive.kernel.org/centos-vault/7.9.2009/updates/$basearch/|g' /etc/yum.repos.d/CentOS-Base.repo
+sed -i 's|baseurl=http://archive.kernel.org/centos-vault/.*./extras//|baseurl=http://archive.kernel.org/centos-vault/7.9.2009/extras/$basearch/|g' /etc/yum.repos.d/CentOS-Base.repo
+sed -i 's|baseurl=http://archive.kernel.org/centos-vault/.*./centosplus//|baseurl=http://archive.kernel.org/centos-vault/7.9.2009/centosplus/$basearch/|g' /etc/yum.repos.d/CentOS-Base.repo
 #sed -i "s/centos_version/${centosV}/g" /etc/yum.repos.d/CentOS-Base.repo
 
 
